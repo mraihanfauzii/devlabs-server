0486b8ee5dea82f4991506895e4ee76853ebe6cf
Rieza Fachrezi <36400438+riezaf51@users.noreply.github.com>
2024-06-30 14:34:54 +0700

    Chore/codebase (#1) (#2)
    * chore: set up codebase depedencies
    
    * chore: update dependency & add gitignore

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.send({
+    success: true,
+    message: 'This service is running properly',
+    code: 200,
+  });
+});
+
+module.exports = router;
