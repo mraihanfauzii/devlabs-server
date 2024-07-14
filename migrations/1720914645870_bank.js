4b825930f47a9544e7e63471234c5109bcd15a65
FARHAN TIRTA KESUMAH <tirta.farhn@gmail.com>
2024-07-14 07:29:30 +0700

    Banks & Payment
    - Add banks and payments model
    - Add banks and payments seeder

---
+++ migrations/1720914645870_bank.js
@@ -0,0 +1,54 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('banks', {
+        id: {
+          type: 'uuid',
+          primaryKey: true,
+          default: pgm.func('uuid_generate_v4()'),
+        },
+        name: {
+          type: 'varchar(100)',
+          notNull: true,
+        },
+        accname: {
+          type: 'varchar(100)',
+          notNull: true,
+        },
+        accnumber: {
+          type: 'varchar(100)',
+          notNull: true,
+        },
+        created_at: {
+          type: 'timestamp',
+          default: pgm.func('current_timestamp'),
+        },
+    });
+    pgm.sql(`
+    INSERT INTO banks (id, name, accName, accNumber) VALUES
+      ('6bccf43e-b413-42e8-86f4-f9312566466c', 'BCA', 'PT Devlab', '1234567890'),
+      ('a0b6dea6-9610-42a1-8b1f-2ffce7566f6d', 'BNI', 'PT Devlab', '0987654321'),
+      ('cb9d62ae-1dbb-4422-ad04-812f80771f8b', 'Mandiri', 'PT Devlab', '3525325325'),
+      ('80376bf5-f806-4df5-8acd-9e353624ad48', 'BSI', 'PT Devlab', '032552167'),
+      ('3a3f6156-6d5b-45b2-9b2e-e5e8f472b6a4', 'VA BNI', 'PT Devlab', '00110987654321'),
+      ('7f1d4b7f-8706-4e1f-a50b-9f0e54b98d0c', 'VA Mandiri', 'PT Devlab', '00113525325325'),
+      ('b365e9e1-9a6b-4a62-92b8-8a1a52f9ae7f', 'VA BSI', 'PT Devlab', '0011032552167')
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('banks');
+};
---
+++ migrations/1720914885060_payment.js
@@ -0,0 +1,50 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('payments', {
+        id: {
+          type: 'uuid',
+          primaryKey: true,
+          default: pgm.func('uuid_generate_v4()'),
+        },
+        type: {
+          type: 'varchar(100)',
+          notNull: true,
+        },
+        banks: {
+          type: 'uuid',
+        },
+        created_at: {
+          type: 'timestamp',
+          default: pgm.func('current_timestamp'),
+        },
+    });
+    pgm.addConstraint('payments', 'fk_banks_id', 'FOREIGN KEY(banks) REFERENCES banks(id) ON DELETE SET NULL');
+    pgm.sql(`
+    INSERT INTO payments (type, banks) VALUES
+      ('Bank Transfer', '6bccf43e-b413-42e8-86f4-f9312566466c'),
+      ('Bank Transfer', 'a0b6dea6-9610-42a1-8b1f-2ffce7566f6d'),
+      ('Bank Transfer', 'cb9d62ae-1dbb-4422-ad04-812f80771f8b'),
+      ('Bank Transfer', '80376bf5-f806-4df5-8acd-9e353624ad48'),
+      ('Virtual Account', '3a3f6156-6d5b-45b2-9b2e-e5e8f472b6a4'),
+      ('Virtual Account', '7f1d4b7f-8706-4e1f-a50b-9f0e54b98d0c'),
+      ('Virtual Account', 'b365e9e1-9a6b-4a62-92b8-8a1a52f9ae7f')
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('payments');
+};
