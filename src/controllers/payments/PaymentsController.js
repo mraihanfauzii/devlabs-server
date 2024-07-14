f600b34a316d07ac6a111730b13011e352c051f1
FARHAN TIRTA KESUMAH <tirta.farhn@gmail.com>
2024-07-14 12:09:15 +0700

    Payment controller
    - get all payment
    - get paymen by id

---
+++ src/controllers/payments/PaymentsController.js
@@ -0,0 +1,30 @@
+
+class PaymentsController {
+  constructor(PaymentsRepository) {
+    this.PaymentsRepository = PaymentsRepository;
+  }
+
+  async getAllPaymentMethod(req, res) {
+    const payment = await this.PaymentsRepository.getAllPaymentMethod();
+
+    return res.status(200).send({
+      success: true,
+      message: 'Payment method found',
+      code: 200,
+      data: payment.rows,
+    });
+  }
+  async getAllPaymentMethodByID(req, res) {
+    const id = req.params
+    const payment = await this.PaymentsRepository.getAllPaymentMethodByID(id);
+
+    return res.status(200).send({
+      success: true,
+      message: 'Payment method by id found',
+      code: 200,
+      data: payment.rows[0],
+    });
+  }
+
+}
+module.exports = PaymentsController;
