f1c99ebf51ed185ceb6fcaf69f31c709913f066a
FARHAN TIRTA KESUMAH <tirta.farhn@gmail.com>
2024-07-16 11:51:35 +0700

    project detail
    - add project detail table
    - alter project

---
+++ migrations/1721097665802_alter-project.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.addColumns('projects', {
+        detail_id: { 
+            type: 'uuid'
+        },
+    });
+    pgm.addConstraint('projects', 'fk_detail', 'FOREIGN KEY(detail_id) REFERENCES projectdetail(id) ON DELETE SET NULL');
+    pgm.dropColumns( 'projects', 'notes' )
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {'projects'};
